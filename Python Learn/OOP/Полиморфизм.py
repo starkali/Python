1e1f43e26a8dea6ec15b3e6e4dcb3594a96af730
Ali Kirilov <122522569+starkali@users.noreply.github.com>
2023-01-30 13:29:01 +0400

    Add files via upload

---
+++ Python Learn/OOP/Полиморфизм.py
@@ -0,0 +1,34 @@
+#Inheritance
+class Car:
+    wheels_number = 4
+    def __init__(self, name, color, year, is_crashed):
+        # Этот метод отвечает за инициализацию экземпляров класса после их создания | Инициализация — приведение цифрового устройства или его программы в состояние готовности к использованию.
+        self.name = name
+        self.color = color
+        self.year = year
+        self.is_crashed = is_crashed
+        print('Car is created')
+        
+    def drive(self, city):
+        print(self.name + ' is driving to ' + city)
+        
+    def change_color(self, new_color):
+        self.color = new_color
+        
+class Truck(Car):
+    wheels_number = 6
+    def __init__(self, name, color, year, is_crashed):
+        Car.__init__(self, name, color, year, is_crashed)
+        print('Truck is created')
+    def drive(self, city):
+        print('Truck ' + self.name + ' is driving to ' + city)
+        
+    def load_cargo(self, weight):
+        print('The cargo is loaded. Weight is ' + str(weight) + ' kg.')
+        
+man_truck = Truck('Man', 'White', 2015, False)
+man_truck.drive('New York')
+print(man_truck.wheels_number)
+man_truck.load_cargo(2000)
+
+# Polymorphism
