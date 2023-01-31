96b21dfe22ae019c6538476ed05893f4215455f6
Ali <alikirilovz@gmail.com>
2023-01-31 11:19:08 +0400

    Add files via upload

---
+++ Python Learn/Modules and Packages/Встроенные модули.py
@@ -0,0 +1,28 @@
+# import random
+# x = random.randint(1, 10)
+# print(x)
+import math
+# from random import randint
+# x = randint(1, 10)
+# print(x)
+#
+# from random import shuffle
+# my_list = [1, 2, 3]
+# shuffle(my_list)
+# print(my_list)
+
+# from random import shuffle as shuffle_my_list
+# my_list = [1, 2, 3]
+# shuffle_my_list(my_list)
+# print(my_list)
+
+import math as m
+
+# Корень квадратный из числа 123456789
+print(m.sqrt(123456789))
+
+# Факториал числа 987
+print(m.factorial(987))
+
+# 876 в степени 54
+print(m.pow(876, 54))
