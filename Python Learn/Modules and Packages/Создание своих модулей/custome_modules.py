4ce04e069f5515deff389abdcd63835049c83eba
Ali <alikirilovz@gmail.com>
2023-01-31 11:40:19 +0400

    Add files via upload

---
+++ Python Learn/Modules and Packages/Создание своих модулей/custome_modules.py
@@ -0,0 +1,9 @@
+# import greetings
+#
+# greetings.say_hello()
+
+from greetings import say_hey_there
+say_hey_there()
+
+from goodbyes import say_good_bye as my_good_bye
+my_good_bye()
---
+++ Python Learn/Modules and Packages/Создание своих модулей/goodbyes.py
@@ -0,0 +1,10 @@
+def say_good_bye():
+    print('Good Bye!')
+
+
+def say_bye_bye():
+    print('Bye-Bye!')
+
+
+def say_hasta_la_vista():
+    print('Hasta la vista!')
