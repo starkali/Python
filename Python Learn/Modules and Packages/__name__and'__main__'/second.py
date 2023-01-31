146495ae1a3980091b23b8e39d988659ee7f36c0
Ali <alikirilovz@gmail.com>
2023-01-31 13:29:42 +0400

    Add files via upload

---
+++ Python Learn/Modules and Packages/__name__and'__main__'/second.py
@@ -0,0 +1,10 @@
+import first
+
+print('Top level in second.py')
+
+first.function_1()
+
+if __name__ == '__main__':
+    print('second.py is being run directly')
+else:
+    print('second.py has been imported')
