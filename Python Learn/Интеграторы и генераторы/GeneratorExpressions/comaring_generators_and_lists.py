b2522a77ff4283ca015f451b2e13865149427d29
Ali <alikirilovz@gmail.com>
2023-02-03 17:24:28 +0400

    Add files via upload

--- Python Learn/Интеграторы и генераторы/GeneratorExpressions/comaring_generators_and_lists.py
+++ Python Learn/Интеграторы и генераторы/GeneratorExpressions/comaring_generators_and_lists.py
@@ -4,11 +4,11 @@ import time
 # print(sum(number for number in range(10)))
 
 list_start_time = time.time()
-print(sum([number for number in range(100000000)]))
+print(sum([number for number in range(9000000)]))
 list_processing_time = time.time() - list_start_time
 
 gen_start_time = time.time()
-print(sum(number for number in range(100000000)))
+print(sum(number for number in range(9000000)))
 gen_processing_time = time.time() - gen_start_time
 
 print(f'Processing with list is {list_processing_time}')
