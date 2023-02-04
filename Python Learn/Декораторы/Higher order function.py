61882e328e253bfa4785615d80504ca6d4070077
Ali <alikirilovz@gmail.com>
2023-02-04 16:27:48 +0400

    Add files via upload

--- Python Learn/Декораторы/Higher order function.py
+++ Python Learn/Декораторы/Higher order function.py
@@ -1,4 +1,5 @@
-# Higher other functions, which accept another functions as arguments
+# Higher order functions, which accept another functions as arguments
+
 
 # def product(n, func):
 #     result = 1
@@ -6,25 +7,84 @@
 #         result *= func(number)
 #     return result
 #
+#
 # def square(x):
 #     return x * x
+#
 #
 # def cube(x):
 #     return x * x * x
 #
+#
 # print(product(4, square))
+# # 1 2 3
+# # 1 4 9
+#
 # print(product(4, cube))
+# # 1 2 3
+# # 1 8 27
 
 
 # def my_function(a, b, func):
 #     result = func([a, b])
 #     return result
-
+#
+#
 # print(my_function(7, 3, sum))
+
 
 # Using nested functions
 
-def colorize(thing):
-    def get_clor():
+
+from random import choice
+
+
+# def colorize(thing):
+#     def get_color():
+#         colors = ('red', 'green', 'yellow')
+#         color = choice(colors)
+#         return color
+#
+#     result = get_color() + ' ' + thing
+#     return result
+#
+#
+# print(colorize('apple'))
+
+# Higher order functions, which return another function
+
+
+# def make_color():
+#     def get_color():
+#         colors = ('red', 'green', 'yellow')
+#         color = choice(colors)
+#         return color
+#
+#     return get_color
+#
+#
+# first_color = make_color()
+# print(first_color())
+#
+# second_color = make_color()
+# print(second_color())
+#
+# third_color = make_color()
+# print(third_color())
+
+
+# Inner functions can access outer function scope
+
+
+def colorize1(thing):
+    def get_color():
         colors = ('red', 'green', 'yellow')
-        color
+        color = choice(colors)
+        return color + ' ' + thing
+
+    return get_color
+
+
+# print(colorize1('apple')())
+colorized_dog = colorize1('dog')
+print(colorized_dog())
