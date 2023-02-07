0b48edd5de31ae5a1187c3e02deee001169b6613
Ali <alikirilovz@gmail.com>
2023-02-07 15:58:25 +0400

    Add files via upload

---
+++ Python Learn/Декораторы/speed_test.py
@@ -0,0 +1,40 @@
+from time import time
+from functools import wraps
+
+
+def speed_test(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        start_time = time()
+        result = function(*args, **kwargs)
+        end_time = time()
+        print(f"Time of code execution {end_time - start_time}")
+        return result
+    return wrapper
+
+
+@speed_test
+def sum_with_list():
+    return sum([number for number in range(10000000)])
+
+
+@speed_test
+def sum_with_gen():
+    return sum(number for number in range(10000000))
+
+
+@speed_test
+def product(range_value):
+    result = 1
+    for number in range(1, range_value):
+        result *= number
+
+    return result
+
+
+print(sum_with_list())
+print(sum_with_gen())
+print(product(100000))
+
+
+
