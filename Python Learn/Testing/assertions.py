1c0e1d5adc46d2f6cdd23024febfa444534cfe22
Ali Kirilov <alikirilovz@gmail.com>
2023-04-25 16:09:18 +0400

    Add files via upload

--- Python Learn/Testing/assertions.py
+++ Python Learn/Testing/assertions.py
@@ -12,9 +12,21 @@
 # print(divide(2, 2))
 # print(divide(0, 3))
 
-def multiply_positive_numbers(a, b):
-    assert a > 0 and b > 0, ' a & b must be positive'
-    print(a * b)
+# def multiply_positive_numbers(a, b):
+#     assert a > 0 and b > 0, ' a & b must be positive'
+#     print(a * b)
+#
+# multiply_positive_numbers(3, 5)
+# multiply_positive_numbers(3, -5)
 
-multiply_positive_numbers(3, 5)
-multiply_positive_numbers(3, -5)
+def get_access(password):
+    password_list = [111, 222, 333]
+    try:
+        password = int(password)
+    except ValueError:
+        return 'Password should be a number'
+    assert password in password_list, 'Password is invalid'
+    print('You can make dangerous stuff')
+
+password_1 = input('Please input the password: ')
+get_access(password_1)
